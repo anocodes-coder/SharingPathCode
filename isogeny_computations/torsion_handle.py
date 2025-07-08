2a750b8436d0283d623a5d665878e7a43874190b
anocodes-coder <anocodes@proton.me>
2025-07-08 18:05:17 -0400

    Update torsion_handle.py

--- isogeny_computations/torsion_handle.py
+++ isogeny_computations/torsion_handle.py
@@ -96,11 +96,11 @@ def Sharing_Data_Computation(params):
 
 
 """
-!!! In the input we have the necessary information to recompute a secret isogeny !!!
+!!! In the input, we have the necessary information to recompute a secret isogeny !!!
        + params gives the finite field over which the elliptic curves are defined.
-	   + E1_params contains the initial curve E1 and the torsion points.
-	   + imgA and imgB represent the images of the torsion points.
-	   + EAB is optional in the context of our scheme.
+       + E1_params contains the initial curve E1 and the torsion points.
+       + imgA and imgB represent the images of the torsion points.
+       + EAB is optional in the context of our scheme.
 """
 def Isogeny_Recomputation(params,E1_params,imgA,imgB,EAB=None):
 	t1=time()
